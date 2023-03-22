236e748e0d0c31d74a72fb30f01882dde51c960b
lionhard83 <carloleonardi83@gmail.com>
2023-03-22 19:38:27 +0100

    add readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# 📱 CASAFARI
+
+Casari is legacy API to casafari services
+
+Created by [@lionhard83](https://github.com/lionhard83),
+
+## How to use
+
+```
+import casafari from "casafari";
+export const cf = casafari("Token YOUR_TOKEN");
+// ...
+cf.getTypes()
+
+```
--- src/index.ts
+++ src/index.ts
@@ -3,34 +3,47 @@ import { Data } from "./models/Apartment";
 import { Feed, FeedPayload, FeedResponseOnCreate } from "./models/Feed";
 import { Type } from "./models/Types";
 const baseUrl = "https://api.casafari.com/v1";
-const config = { headers: { Authorization: "" } };
 
-export const setToken = (token: string) => {
+export default function (token: string) {
+  const config = { headers: { Authorization: "" } };
   config.headers.Authorization = token;
-};
 
-export const getTypes = async () =>
-  (await axios.get<Type[]>(`${baseUrl}/references/types`, config)).data;
+  const setToken = (token: string) => {
+    config.headers.Authorization = token;
+  };
+  const getTypes = async () =>
+    (await axios.get<Type[]>(`${baseUrl}/references/types`, config)).data;
 
-export const getFeeds = async () =>
-  (await axios.get<Feed[]>(`${baseUrl}/listing-alerts/feeds`, config)).data;
+  const getFeeds = async () =>
+    (await axios.get<Feed[]>(`${baseUrl}/listing-alerts/feeds`, config)).data;
 
-export const createFeed = async (body: FeedPayload) =>
-  (
-    await axios.post<FeedResponseOnCreate>(
-      `${baseUrl}/listing-alerts/feeds`,
-      body,
-      config
-    )
-  ).data;
+  const createFeed = async (body: FeedPayload) =>
+    (
+      await axios.post<FeedResponseOnCreate>(
+        `${baseUrl}/listing-alerts/feeds`,
+        body,
+        config
+      )
+    ).data;
 
-export const deleteFeed = async (id: number) =>
-  (
-    await axios.delete<{ success: boolean }>(
-      `${baseUrl}/listing-alerts/feeds/${id}`,
-      config
-    )
-  ).data;
+  const deleteFeed = async (id: number) =>
+    (
+      await axios.delete<{ success: boolean }>(
+        `${baseUrl}/listing-alerts/feeds/${id}`,
+        config
+      )
+    ).data;
 
-export const getFeed = async (id: number) =>
-  (await axios.get<Data>(`${baseUrl}/listing-alerts/feeds/${id}`, config)).data;
+  const getFeed = async (id: number) =>
+    (await axios.get<Data>(`${baseUrl}/listing-alerts/feeds/${id}`, config))
+      .data;
+
+  return {
+    getFeed,
+    deleteFeed,
+    createFeed,
+    getFeeds,
+    getTypes,
+    setToken,
+  };
+}
